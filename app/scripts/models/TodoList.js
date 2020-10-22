6732863731e176c1d5dc6da769a8076ef6b38c71
alvin <alvinhtml@gmail.com>
2020-10-22 18:10:06 +0800

    added Todo Model

--- app/scripts/models/TodoList.js
+++ app/scripts/models/TodoList.js
@@ -1,1 +1,22 @@
 /* @flow */
+
+import {Record, List, Map} from 'immutable';
+
+export class Todo extends Record({
+  id: 0,
+  title: ''
+}) {
+  id: number;
+  title: string;
+
+  get title(): string {
+    return title;
+  }
+}
+
+export function toTodo(todo: Object) {
+  return new Todo({
+    id: todo.id,
+    title: todo.title || '--'
+  });
+}
